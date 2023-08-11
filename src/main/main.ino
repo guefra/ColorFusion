04d28902b6db7224b52d728440d892a1e23fe262
guefra <123297340+guefra@users.noreply.github.com>
2023-08-11 21:43:37 +0200

    Uncomment values for other potentiometers and add ability to print 'HSV' values

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,6 +1,6 @@
 int val_pot_H;
-// int val_pot_S;
-// int val_pot_V;
+int val_pot_S;
+int val_pot_V;
 
 void setup() {
   Serial.begin(9600);
@@ -8,10 +8,14 @@ void setup() {
 
 void loop() {
   val_pot_H = analogRead(A0);
-  // val_pot_S = analogRead(A1);
-  // val_pot_V = analogRead(A2);
-  Serial.println(val_pot_H);
-  // Serial.println(val_pot_S);
-  // Serial.println(val_pot_V);
+  val_pot_S = analogRead(A1);
+  val_pot_V = analogRead(A2);
+
+  Serial.print(val_pot_H);
+  Serial.print(", ");
+  Serial.print(val_pot_S);
+  Serial.print(", ");
+  Serial.println(val_pot_V);
+
   delay(100);
 }
