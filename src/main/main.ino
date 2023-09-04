03c1f53d75c0db107fcb4bd721d541e7b9e5e6e9
guefra <123297340+guefra@users.noreply.github.com>
2023-09-04 17:32:27 +0200

    Setup led strip

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,15 +1,24 @@
+#include <Adafruit_NeoPixel.h>
+
+#define LED_PIN    6 // The pin where the data input of the RGB strip goes
+#define LED_COUNT 71 // The amount of leds on the strip
+
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+
 int val_pot_H;
 int val_pot_S;
 int val_pot_V;
 
 void setup() {
   Serial.begin(9600);
+  strip.begin();
+  strip.show();
+  strip.setBrightness(50);
 }
 
 void loop() {
   readValues();
   // printValues();
-
   delay(100);
 }
 
@@ -20,9 +29,9 @@ void readValues() {
 }
 
 // void printValues() {
-  // Serial.print(val_pot_H);
-  // Serial.print(", ");
-  // Serial.print(val_pot_S);
-  // Serial.print(", ");
-  // Serial.println(val_pot_V);
+//   Serial.print(val_pot_H);
+//   Serial.print(", ");
+//   Serial.print(val_pot_S);
+//   Serial.print(", ");
+//   Serial.println(val_pot_V);
 // }
