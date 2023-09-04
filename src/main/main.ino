3e2fa3c9fd8f4aaf97a6950bb9d443c9a4dcdac1
guefra <123297340+guefra@users.noreply.github.com>
2023-09-04 18:52:10 +0200

    Convert values of potentiometers in HSV values

--- src/main/main.ino
+++ src/main/main.ino
@@ -1,13 +1,16 @@
 #include <Adafruit_NeoPixel.h>
 
-#define LED_PIN    6 // The pin where the data input of the RGB strip goes
-#define LED_COUNT 71 // The amount of leds on the strip
+#define LED_PIN    6 // The pin where the data input of the RGB strip goes.
+#define LED_COUNT 71 // The amount of leds on the strip.
 
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 int val_pot_H;
 int val_pot_S;
 int val_pot_V;
+uint16_t h;
+uint8_t s;
+uint8_t v;
 
 void setup() {
   Serial.begin(9600);
@@ -18,8 +21,9 @@ void setup() {
 
 void loop() {
   readValues();
-  // printValues();
-  delay(100);
+  convertHSV();
+  printNewValues();
+  //printValues();
 }
 
 void readValues() {
@@ -28,10 +32,24 @@ void readValues() {
   val_pot_V = analogRead(A2);
 }
 
-// void printValues() {
-//   Serial.print(val_pot_H);
-//   Serial.print(", ");
-//   Serial.print(val_pot_S);
-//   Serial.print(", ");
-//   Serial.println(val_pot_V);
-// }
+void printValues() {
+  Serial.print(val_pot_H);
+  Serial.print(", ");
+  Serial.print(val_pot_S);
+  Serial.print(", ");
+  Serial.println(val_pot_V);
+}
+
+void convertHSV() {
+  h = round(val_pot_H * 64.0615835777); // Between 0 and 65535.
+  s = round(val_pot_S / 4.01176470588); // Between 0 and 255.
+  v = round(val_pot_V / 4.01176470588); // Between 0 and 255.
+}
+
+void printNewValues() {
+  Serial.print(h);
+  Serial.print(", ");
+  Serial.print(s);
+  Serial.print(", ");
+  Serial.println(v);
+}
